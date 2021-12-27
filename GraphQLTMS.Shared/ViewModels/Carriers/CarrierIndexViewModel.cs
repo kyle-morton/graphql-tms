264b5c26484cce5272c5a1ab6cc458e9c0202746
Kyle Morton <kylemortondevelopment@gmail.com>
2021-12-27 00:25:12 -0600

    Added carriers index to graphQL

---
+++ GraphQLTMS.Shared/ViewModels/Carriers/CarrierIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLTMS.Shared.ViewModels.Carriers
+{
+    public class CarrierIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Scac { get; set; }
+    }
+}
---
+++ GraphQLTMS.Shared/ViewModels/Carriers/GetCarrierIndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphQLTMS.Shared.ViewModels.Carriers
+{
+    public class GetCarrierIndexViewModel
+    {
+        public List<CarrierIndexViewModel> Carriers { get; set; }
+    }
+}
